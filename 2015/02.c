401d917303e343174bc8b58871cb8b87b432c898
Bryon Meinka <bryon@meinka.net>
2020-10-16 10:14:58 -0400

    refactor 2025/02
    - use macros instead of one-line functions
    - use a pointer instead of copying data by returning a struct

--- 2015/02.c
+++ 2015/02.c
@@ -1,48 +1,43 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define min(a, b) a < b ? a : b
+#define max(a, b) a > b ? a : b
+#define box_volume(b) b.w * b.h * b.l
+#define box_area(b) 2 * (b.w * b.h + b.w * b.l + b.h * b.l)
+
 struct box { int w, h, l; };
 
-struct box parse_box(char *line) {
-	struct box box;
+/* parse the dimensions of a box from `line` and fill them into `box` */
+bool parse_box(struct box *box, char *line) {
 	int a, b, c;
 	if (sscanf(line, "%dx%dx%d\n", &a, &b, &c) != 3) {
 		printf("error parsing box: %s\n", line);
-		exit(1);
+		return false;
 	}
 	// ensure w*h is always the smallest face
 	if (a < b) {
-		box.w = a;
-		box.h = b < c ? b : c;
-		box.l = b < c ? c : b;
+		box->w = a;
+		box->h = min(b, c);
+		box->l = max(b, c);
 	} else 	{
-		box.w = b;
-		box.h = a < c ? a : c;
-		box.l = a < c ? c : a;
+		box->w = b;
+		box->h = min(a, c);
+		box->l = max(a, c);
 	}
-	return box;
-}
-
-int box_area(struct box b) {
-	return ((b.w * b.h) + (b.w * b.l) + (b.h * b.l)) * 2;
-}
-
-int box_volume(struct box b) {
-	return b.w * b.h * b.l;
-}
-
-void print_box(struct box box) {
-	printf("Box(w=%d,h=%d,l=%d)\n", box.w, box.h, box.l);
+	return true;
 }
 
 int main(void) {
 	char line[1024];
+	struct box box;
 
 	int paper = 0;
 	int ribbon = 0;
 
 	while(fgets(line, sizeof line, stdin) != NULL) {
-		struct box box = parse_box(line);
+		if (!parse_box(&box, line)) return 1;
 		paper += box_area(box);
 		paper += (box.w * box.h);
 		ribbon += box_volume(box);
