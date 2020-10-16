b23b1b7834fc96353276025ec6637cb3dc4a9080
Bryon Meinka <bryon@meinka.net>
2020-10-16 11:33:00 -0400

    update 2015/01
    - rename variable `floor` to `position`

--- 2015/01.c
+++ 2015/01.c
@@ -2,20 +2,20 @@
 
 int main(void) {
 	int c;
-	int floor = 0;
+	int position = 0;
 	int basement_step = 0;
 	int step = 0;
 
 	while ((c = getc(stdin)) != EOF) {
 		if (c == '(')
-			floor++;
+			position++;
 		else if (c == ')')
-			floor--;
+			position--;
 		step++;
-		if (basement_step == 0 && floor < 0)
+		if (basement_step == 0 && position < 0)
 			basement_step = step;
 	}
-	printf("part one: %d\n", floor);
+	printf("part one: %d\n", position);
 	printf("part two: %d\n", basement_step);
 	return 0;
 }
